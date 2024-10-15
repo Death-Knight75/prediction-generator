26d9da277485b3899c40f24b6e65b5a371b87943
Carsten <liebesgott1975@googlemail.com>
2024-10-15 10:59:19 +0200

    Adding Folder and Files to Projekt

---
+++ terraform/modules/variables.tf
@@ -0,0 +1,9 @@
+variable "role_name" {      
+    description = "The name of the Lambda execution role."
+    type = string
+}
+
+variable "policy_name" {
+    description = "The name of the Lambda execution policy."
+    type = string
+}
