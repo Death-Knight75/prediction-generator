0aecc7abf1c8c0f61036c608beb82577ec2b001d
Carsten <liebesgott1975@googlemail.com>
2024-10-15 11:23:08 +0200

    change files

--- terraform/modules/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role_name" {      
-    description = "The name of the Lambda execution role."
-    type = string
-}
-
-variable "policy_name" {
-    description = "The name of the Lambda execution policy."
-    type = string
-}
